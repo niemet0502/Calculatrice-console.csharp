0cccd3b67e396d3150940ae118523e932d8e27d5
niemet0502 <vincentmarius8@gmail.com>
2020-12-18 18:31:08 +0100

    added calculatrice code

--- CalculatriceConsoleCshap/CalculatriceConsoleCshap/Program.cs
+++ CalculatriceConsoleCshap/CalculatriceConsoleCshap/Program.cs
@@ -4,19 +4,76 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CalculatriceConsoleCshap
+namespace ExerciceLgl
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Cette app est une calculatrice");
-            Console.WriteLine("A: Addition");
-            Console.WriteLine("M: Multiplication");
-            Console.WriteLine("S: Soustraction");
-            Console.WriteLine("D: Division");
-            Console.WriteLine("Choisissez une operation");
-            char x =  char.Parse(String.ToUpper(Console.ReadLine()));
+            string cal;
+            do
+            {
+                int choix = 0, nb1 = 0, nb2 = 0;
+                Console.WriteLine("\t\t\tcalculatrice");
+                Console.WriteLine("\t\t-------------MENU-------------");
+                Console.WriteLine("\t\t1-------------------- addition");
+                Console.WriteLine("\t\t2---------------- soustraction");
+                Console.WriteLine("\t\t3---------------multiplication");
+                Console.WriteLine("\t\t4---------------------division");
+                Console.WriteLine("\t\t5----------------------quitter");
+                Console.WriteLine(" ");
+                Console.WriteLine("entrer le premier nombre");
+                nb1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("entrer le deuxieme nombre");
+                nb2 = int.Parse(Console.ReadLine());
+                do
+                {
+
+                    Console.WriteLine("choisissez votre operation");
+                    choix = int.Parse(Console.ReadLine());
+
+                } while (!(choix >= 1 && choix <= 5));
+
+
+                switch (choix)
+                {
+                    case 1:
+                        Console.WriteLine(nb1 + "+" + nb2 + "=" + (nb1 + nb2));
+                        break;
+                    case 2:
+                        Console.WriteLine(nb1 + "-" + nb2 + "=" + (nb1 - nb2));
+                        break;
+                    case 3:
+                        Console.WriteLine(nb1 + "*" + nb2 + "=" + (nb1 * nb2));
+                        break;
+                    case 4:
+                        if (nb2 == 0)
+                        {
+                            Console.WriteLine("operation impossible");
+                        }
+                        else
+                        {
+                            Console.WriteLine(nb1 + "/" + nb2 + "=" + (nb1 / nb2));
+                        }
+
+                        break;
+                    case 5:
+                        Console.WriteLine("Aurevoir...");
+                        break;
+
+
+
+                }
+                Console.WriteLine("voulez vous faire un autre calcul?[o/n]");
+                cal = Console.ReadLine();
+                Console.Clear();
+            } while (cal == "o" || cal == "O");
+            if (cal == "n" || cal == "N")
+            {
+                Console.WriteLine("\t\t\t\t-----fin du calcul-----");
+            }
+
+            Console.ReadLine();
         }
     }
 }
