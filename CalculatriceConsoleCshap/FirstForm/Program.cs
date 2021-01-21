e8148092550fc47c36c69b4b80dfc58878da46a7
niemet0502 <vincentmarius8@gmail.com>
2021-01-21 21:00:29 +0100

    main :: init main Program file

--- CalculatriceConsoleCshap/FirstForm/Program.cs
+++ CalculatriceConsoleCshap/FirstForm/Program.cs
@@ -17,7 +17,7 @@ namespace FirstForm
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmVille());
+            Application.Run(new frmDropBox());
         }
     }
 }
